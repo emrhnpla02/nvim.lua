9e0420afb3026fdc08c312a0c0a89670b38b6aed
Emirhan Pala <emrhnpla02@protonmail.com>
2022-06-12 01:04:32 +0300

    Save folds when abondon the file

--- lua/emrhnpla/options.lua
+++ lua/emrhnpla/options.lua
@@ -42,3 +42,12 @@ end
 vim.cmd("set whichwrap+=<,>,[,],h,l")
 vim.cmd([[set iskeyword+=-,/]])
 vim.cmd([[set formatoptions-=cro]])
+vim.cmd([[set viewoptions-=options ]])
+
+vim.cmd([[
+augroup remember_folds
+    autocmd!
+    autocmd BufWinLeave *.* mkview
+    autocmd BufWinEnter *.* silent! loadview
+augroup END
+]])
